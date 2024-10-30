46849236bc564f6921057b1ef82e4014a9a03fd1
filpedroso <filpedroso3@gmail.com>
2024-10-30 13:01:12 +0000

    ft_strlcpy

--- common-core/libft/libft.h
+++ common-core/libft/libft.h
@@ -6,15 +6,20 @@
 /*   By: fpedroso <fpedroso@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 16:12:28 by fpedroso          #+#    #+#             */
-/*   Updated: 2024/10/27 16:16:56 by fpedroso         ###   ########.fr       */
+/*   Updated: 2024/10/29 21:24:21 by fpedroso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
+# include <stdio.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 
 #endif
